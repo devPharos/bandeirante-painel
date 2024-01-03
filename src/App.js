c4411eb6e60c3cd47f4699eff883e1d9afb9a691
Thayná Gitirana <thayna@pharosit.com.br>
2024-01-03 16:26:22 -0300

    feat: add dynamic data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,98 @@
-import {
-  ArrowBigRight,
-  Clock,
-  LayoutTemplate,
-  ListChecks,
-  MoveRight,
-} from 'lucide-react'
+import { Clock, LayoutTemplate, ListChecks, MoveRight } from 'lucide-react'
 import logo from './assets/logo.png'
+import { useEffect, useState } from 'react'
+import { format } from 'date-fns'
+
 export default function App() {
+  const [date, setDate] = useState(format(new Date(), 'HH:mm'))
+  const [requests, setRequests] = useState([])
+  const [picking, setPicking] = useState([])
+
+  const leftBoardData = [
+    {
+      code: '123456',
+      name: 'CENTRO REAL',
+      date: new Date(),
+      quantity: 45,
+      status: 'Iniciado',
+      responsible: 'Jefferson Melo',
+      pct: 35,
+    },
+    {
+      code: '123457',
+      name: 'CENTRO REAL',
+      date: new Date(2023, 1, 12),
+      quantity: 46,
+      status: 'NaoIniciado',
+      responsible: 'Jefferson Melo',
+    },
+    {
+      code: '123458',
+      name: 'CENTRO REAL',
+      date: new Date(2023, 1, 12),
+      quantity: 46,
+      status: 'NaoIniciado',
+      responsible: 'Jefferson Melo',
+    },
+    {
+      code: '123459',
+      name: 'CENTRO REAL',
+      date: new Date(2023, 1, 12),
+      quantity: 46,
+      status: 'NaoIniciado',
+      responsible: 'Jefferson Melo',
+    },
+    {
+      code: '12349',
+      name: 'CENTRO REAL',
+      date: new Date(2023, 1, 12),
+      quantity: 46,
+      status: 'NaoIniciado',
+      responsible: 'Jefferson Melo',
+    },
+    {
+      code: '1256',
+      name: 'CENTRO REAL',
+      date: new Date(2023, 1, 12),
+      quantity: 46,
+      status: 'NaoIniciado',
+      responsible: 'Jefferson Melo',
+    },
+  ]
+  const rightBoardData = [
+    {
+      leftCode: 'E6501',
+      mainCode: '7360081E',
+      rightCode: 'A2400',
+      leftData: 30,
+      rightData: 4,
+    },
+  ]
+
+  const handleUpdateLeftBoard = () => {
+    setRequests(leftBoardData)
+  }
+
+  const handleUpdateRightBoard = () => {
+    setPicking(rightBoardData)
+  }
+
+  useEffect(() => {
+    const fetchDados = async () => {
+      const actualDate = format(new Date(), 'HH:mm')
+
+      setDate(actualDate)
+      handleUpdateLeftBoard()
+      handleUpdateRightBoard()
+    }
+
+    fetchDados()
+
+    const intervalId = setInterval(fetchDados, 15000)
+
+    return () => clearInterval(intervalId)
+  }, [])
+
   return (
     <div className="container">
       <main className="content">
@@ -17,50 +103,64 @@ export default function App() {
           </header>
 
           <main className="board-content">
-            <div className="board-row">
-              <section id="board-card" className="in-progress">
-                <span className="card-title">127312</span>
-                <div className="card-content">
-                  <span className="card-info-title">CENTRO REAL</span>
-                  <div className="card-info">
-                    <span>05/01/2024</span> - <span>08:15</span>
-                  </div>
-                </div>
-                <span className="card-quantity">45 CX.</span>
-              </section>
+            {requests &&
+              requests.map((request, index) => {
+                if (index <= 5) {
+                  return (
+                    <div className="board-row" key={request.code}>
+                      <section
+                        id="board-card"
+                        className={
+                          request.status === 'Iniciado'
+                            ? 'in-progress'
+                            : 'to-do'
+                        }
+                      >
+                        <span className="card-title">{request.code}</span>
+                        <div className="card-content">
+                          <span className="card-info-title">
+                            {request.name}
+                          </span>
+                          <div className="card-info">
+                            <span>
+                              {format(request.date, 'dd/MM/yyyy - HH:mm')}
+                            </span>
+                          </div>
+                        </div>
+                        <span className="card-quantity">45 CX.</span>
+                      </section>
 
-              <span id="badge" className="in-progress">
-                Jefferson Melo
-              </span>
+                      <span
+                        id="badge"
+                        className={
+                          request.status === 'Iniciado'
+                            ? 'in-progress'
+                            : 'to-do'
+                        }
+                      >
+                        {request.status === 'Iniciado'
+                          ? request.responsible
+                          : 'Não iniciado'}
+                      </span>
 
-              <span className="data">35%</span>
-            </div>
-
-            <div className="board-row">
-              <section id="board-card" className="to-do">
-                <span className="card-title">127312</span>
-                <div className="card-content">
-                  <span className="card-info-title">CENTRO REAL</span>
-                  <div className="card-info">
-                    <span>05/01/2024</span> - <span>08:15</span>
-                  </div>
-                </div>
-                <span className="card-quantity">45 CX.</span>
-              </section>
-
-              <span id="badge" className="to-do">
-                Não iniciado
-              </span>
-
-              <span className="data"></span>
-            </div>
+                      <span className="data">
+                        {request.pct ? request.pct + '%' : ''}
+                      </span>
+                    </div>
+                  )
+                } else {
+                  return null
+                }
+              })}
           </main>
 
-          <div className="board-footer">
-            <section className="quantity-badge">
-              <span>+3</span>
-            </section>
-          </div>
+          {requests.length > 5 && (
+            <div className="board-footer">
+              <section className="quantity-badge">
+                <span>+{requests.length - 5}</span>
+              </section>
+            </div>
+          )}
         </section>
 
         <section className="board">
@@ -70,33 +170,36 @@ export default function App() {
           </header>
 
           <main className="board-content">
-            <section className="picking-card">
-              <header>
-                <div className="picking-card-info">
-                  <span>E6501</span>
-                </div>
-                <div className="picking-card-info">
-                  <span className="picking-code">7360081E</span>
-                </div>
-                <div className="picking-card-info">
-                  <span>A2400</span>
-                </div>
-              </header>
+            {picking &&
+              picking.map((pc) => (
+                <section className="picking-card" key={pc.mainCode}>
+                  <header>
+                    <div className="picking-card-info">
+                      <span>{pc.leftCode}</span>
+                    </div>
+                    <div className="picking-card-info">
+                      <span className="picking-code">{pc.mainCode}</span>
+                    </div>
+                    <div className="picking-card-info">
+                      <span>{pc.rightCode}</span>
+                    </div>
+                  </header>
 
-              <main>
-                <div className="picking-card-info">
-                  <span>30</span>
-                </div>
+                  <main>
+                    <div className="picking-card-info">
+                      <span>{pc.leftData}</span>
+                    </div>
 
-                <div className="picking-card-info">
-                  <MoveRight size={28} />
-                </div>
+                    <div className="picking-card-info">
+                      <MoveRight size={28} />
+                    </div>
 
-                <div className="picking-card-info">
-                  <span>4</span>
-                </div>
-              </main>
-            </section>
+                    <div className="picking-card-info">
+                      <span>{pc.rightData}</span>
+                    </div>
+                  </main>
+                </section>
+              ))}
           </main>
         </section>
       </main>
@@ -104,7 +207,7 @@ export default function App() {
       <footer className="footer">
         <section className="time">
           <Clock />
-          <span className="time-info">10:35</span>
+          <span className="time-info">{date}</span>
         </section>
         <img src={logo} alt="logo" className="logo" />
       </footer>
